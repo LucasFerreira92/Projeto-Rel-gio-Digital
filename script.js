92da1512aad97555b7fd7d10842d81e6dfb21417
LucasFerreira92 <lucasferreirasurf13@gmail.com>
2024-08-29 17:57:14 -0300

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+const hour = document.querySelector('#hour');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+
+setInterval(() => {
+    let date = new Date();
+    let hours = 
+    let minutes = 
+    let seconds = 
+}
