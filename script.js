f0bc1274aa6e5f5aab24266ea16890d1d5474a65
LucasFerreira92 <lucasferreirasurf13@gmail.com>
2024-08-30 10:18:55 -0300

    update

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const sec = document.querySelector('#sec');
 
 setInterval(() => {
     let date = new Date();
-    let hours = 
-    let minutes = 
-    let seconds = 
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = getSeconds();
+
+    hour.innerHTML = '${formaTime(hours)}';
+    min.innerHTML = '${formaTime(minutes)}';
+    sec.innerHTML = '$formaTime(seconds)}';
+
+}, 1000);
+
+function formaTime(time){
+    return time < 10 ? "0" + time : time;
 }
